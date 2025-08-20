4eac05d299417a7a45a396065d0ae46f19a7b46d
Dhaval Parmar <dhavalparmar3264@gmail.com>
2025-08-20 18:16:41 +0530

    update seed.js to use MongoDB Atlas env variable

--- init/seed.js
+++ init/seed.js
@@ -2,21 +2,19 @@ const mongoose = require("mongoose");
 const Listing = require("../models/listing.js");
 const { data } = require("./data.js");
 
-const MONGO_URL = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = process.env.ATLAS_DB_URL;
 
 main();
 
 async function main() {
   try {
     await mongoose.connect(MONGO_URL);
-    console.log("MongoDB connected.");
-
     await Listing.deleteMany({});
-    console.log("Old listings deleted.");
-
-    await Listing.insertMany(data);
-    console.log("Sample listings inserted.");
-
+    const seededData = data.map((object) => ({
+      ...object,
+      owner: "68610be9df5b932fed3deb0b",
+    }));
+    await Listing.insertMany(seededData);
     mongoose.connection.close();
   } catch (err) {
     console.error("Error seeding DB:", err);
