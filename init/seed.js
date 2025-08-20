f013015dbadd25c91e3ebaf0d72b55c14c7efa6d
Dhaval Parmar <dhavalparmar3264@gmail.com>
2025-08-20 18:08:53 +0530

    i
    B

--- init/seed.js
+++ init/seed.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Listing = require("../models/listing.js");
 const { data } = require("./data.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/wanderlust";
 
 main();
 
